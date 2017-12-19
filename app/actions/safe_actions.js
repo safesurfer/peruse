5005e166bec77b13d0d6257cdfe5b70aba2947c5
Josh Wilson <joshuef@gmail.com>
2017-12-19 12:08:08 -0600

    WIP: safe actions

---
+++ app/actions/safe_actions.js
@@ -0,0 +1,35 @@
+import { createActions } from 'redux-actions';
+
+export const TYPES = {
+    AUTHORISE_SAFE_APP           : 'AUTHORISE_SAFE_APP',
+    GET_BROWSER_CONFIG           : 'GET_BROWSER_CONFIG',
+    SAVE_BROWSER_CONFIG          : 'SAVE_BROWSER_CONFIG',
+    SAVE_BROWSER_CONFIG_AND_QUIT : 'SAVE_BROWSER_CONFIG_AND_QUIT',
+
+    SET_INITIALIZER_TASK        : 'SET_INITIALIZER_TASK',
+    STORE_NEW_ACCOUNT           : 'STORE_NEW_ACCOUNT',
+    RECONNECT_SAFE_APP          : 'RECONNECT_SAFE_APP',
+    SAFE_NETWORK_STATUS_CHANGED : 'SAFE_NETWORK_STATUS_CHANGED'
+};
+
+export const {
+    authoriseSafeApp,
+    getBrowserConfig,
+    saveBrowserConfig,
+    saveBrowserConfigAndQuit,
+
+    setInitializerTask,
+    storeNewAccount,
+    safeNetworkStatusChanged,
+    reconnectSafeApp
+} = createActions(
+    TYPES.AUTHORISE_SAFE_APP,
+    TYPES.GET_BROWSER_CONFIG,
+    TYPES.SAVE_BROWSER_CONFIG,
+    TYPES.SAVE_BROWSER_CONFIG_AND_QUIT,
+
+    TYPES.SET_INITIALIZER_TASK,
+    TYPES.STORE_NEW_ACCOUNT,
+    TYPES.SAFE_NETWORK_STATUS_CHANGED,
+    TYPES.RECONNECT_SAFE_APP
+);
---
+++ test/actions/safe.spec.js
@@ -0,0 +1,76 @@
+import * as safe from 'actions/safe_actions';
+
+console.log( 'safe', safe );
+describe( 'notification actions', () =>
+{
+    it( 'should have types', () =>
+    {
+        expect( safe.TYPES ).toBeDefined();
+    } );
+
+    it( 'should authoriseSafeApp', () =>
+    {
+        const expectedAction = {
+            type : safe.TYPES.AUTHORISE_SAFE_APP
+        };
+        expect( safe.authoriseSafeApp( ) ).toEqual( expectedAction );
+    } );
+
+    it( 'should getBrowserConfig', () =>
+    {
+        const expectedAction = {
+            type : safe.TYPES.GET_BROWSER_CONFIG
+        };
+        expect( safe.getBrowserConfig( ) ).toEqual( expectedAction );
+    } );
+
+    it( 'should saveBrowserConfig', () =>
+    {
+        const expectedAction = {
+            type : safe.TYPES.SAVE_BROWSER_CONFIG
+        };
+        expect( safe.saveBrowserConfig( ) ).toEqual( expectedAction );
+    } );
+
+    it( 'should saveBrowserConfigAndQuit', () =>
+    {
+        const expectedAction = {
+            type : safe.TYPES.SAVE_BROWSER_CONFIG_AND_QUIT
+        };
+        expect( safe.saveBrowserConfigAndQuit( ) ).toEqual( expectedAction );
+    } );
+
+    it( 'should setInitializerTask', () =>
+    {
+        const payload = 'something';
+        const expectedAction = {
+            type : safe.TYPES.SET_INITIALIZER_TASK,
+            payload
+        };
+        expect( safe.setInitializerTask( payload ) ).toEqual( expectedAction );
+    } );
+
+    it( 'should STORE_NEW_ACCOUNT', () =>
+    {
+        const expectedAction = {
+            type : safe.TYPES.STORE_NEW_ACCOUNT
+        };
+        expect( safe.storeNewAccount( ) ).toEqual( expectedAction );
+    } );
+
+    it( 'should safeNetworkStatusChanged', () =>
+    {
+        const expectedAction = {
+            type : safe.TYPES.SAFE_NETWORK_STATUS_CHANGED
+        };
+        expect( safe.safeNetworkStatusChanged( ) ).toEqual( expectedAction );
+    } );
+
+    it( 'should reconnectSafeApp', () =>
+    {
+        const expectedAction = {
+            type : safe.TYPES.RECONNECT_SAFE_APP
+        };
+        expect( safe.reconnectSafeApp( ) ).toEqual( expectedAction );
+    } );
+} );
